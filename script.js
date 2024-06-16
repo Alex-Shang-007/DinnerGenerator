fac8ca93050c78722bc60e8437dfc9dce5623a77
Alex-Shang-007 <alexshang9986@gmail.com>
2024-06-16 01:23:15 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function chooseDinner() {
-    const dinners = ["Spaghetti", "Tacos", "Sushi", "Pizza", "Salad", "Burger", "Steak"];
-    const choice = dinners[Math.floor(Math.random() * dinners.length)];
-    document.getElementById('dinnerChoice').innerText = "Tonight's dinner: " + choice;
-}
